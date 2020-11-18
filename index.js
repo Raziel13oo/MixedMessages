3b988a1fd753b7ee97048bf404d5c7202333ba4c
Diego <rami4054@gmail.com>
2020-11-18 15:19:11 -0500

    Add items to 3 of 4 arrays

---
+++ index.js
@@ -0,0 +1,31 @@
+const professions = [
+  "doctor",
+  "plumber",
+  "painter",
+  "clown",
+  "wizard",
+  "ghost",
+  "baker",
+  "dentist",
+  "scientist",
+];
+const animals = [
+  "cat",
+  "tardigrade",
+  "yoda",
+  "squirtle",
+  "sloth",
+  "dog",
+  "pig",
+  "beholder",
+  "unicorn",
+  "kiwi",
+];
+const other = [];
+const punchline = [
+  "The fourth one ducks.",
+  "The bartender says, What is this, a joke?",
+  "If you want punch you'll have to stand in line, says the bartender. They look around but there is no punchline.",
+  "Ow!, they say.",
+  "It leaves a mean bruise",
+];
